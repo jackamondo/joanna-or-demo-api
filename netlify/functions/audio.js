1e52d941976eae4e9d7d40388a81a3b85ef5f464
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-02-06 17:06:00 +0000

    Update audio.js

--- netlify/functions/audio.js
+++ netlify/functions/audio.js
@@ -1,1 +1,38 @@
-console.log
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // CORS support
+    },
+    body: JSON.stringify({
+      "customer": {
+        "customerName": "Louise Crosby",
+        "customerEmail": "louise.crosby@appamondo.com",
+        "customerPhone": "+44 9485 472 128",
+        "billingAddress": {
+          "street": "47 Park Lane",
+          "city": "London",
+          "postCode": "LND 4A2",
+          "country": "United Kingdom"
+        },
+        "shippingAddress": {
+          "street": "47 Park Lane",
+          "city": "London",
+          "postCode": "LND 4A2",
+          "country": "United Kingdom"
+        },
+        "order": [
+          {
+            "orderDate": "06/02/2025",
+            "orderDetails": [
+              {
+                "
+              }
+            ]
+          }
+        ]
+      }
+    })
+  };
+}; 
