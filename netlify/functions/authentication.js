bb7fc1e8374e572f17182ade6075209854656209
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-02-25 16:40:35 +0000

    Create authentication.js

---
+++ netlify/functions/authentication.js
@@ -0,0 +1,16 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // CORS support
+    },
+    body: JSON.stringify({
+      "profile": {
+        "customerName": "Louise Crosby",
+        "customerEmail": "louise.crosby@appamondo.com",
+        "customerPhone": "+44 9485 472 128"
+      }
+    })
+  };
+}; 
