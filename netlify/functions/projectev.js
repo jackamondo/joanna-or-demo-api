767d6d7b49777fb25edb8c850fe572a5fee7bd1c
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-03-13 15:43:06 +0000

    Update projectev.js

--- netlify/functions/projectev.js
+++ netlify/functions/projectev.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
         "customerPhone": "+44 9485 472 128",
         "accountNumber": "123456789"
       },
-      "products": {[
+      "products": [
           "product": {
             "productName": "7.3kWPro Earth RFID Charger",
             "image": "https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_768/https://www.projectev.co.uk/wp-content/uploads/2022/04/7kW-RFID-Tethered-4-PNG.png",
@@ -22,7 +22,7 @@ exports.handler = async (event, context) => {
           "productName": "APEX-22S-T-4G",
           "image": "https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_768/https://www.projectev.co.uk/wp-content/uploads/2024/03/APEX-Front-Small.webp",
           "purhcaseDate" : "26/11/2024"
-        ]}
+        ]
       }
     })
   };
