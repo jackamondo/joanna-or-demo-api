5a508b68f1a3b1fd9c7204dbd3f02ab5e510d597
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-05-02 11:39:22 +0100

    Update carehomes.js

--- netlify/functions/carehomes.js
+++ netlify/functions/carehomes.js
@@ -6,13 +6,87 @@ exports.handler = async (event, context) => {
       'Access-Control-Allow-Origin': '*', // CORS support
     },
     body: JSON.stringify({
-      "profile": {
-        "customerName": "Louise Crosby",
-        "customerEmail": "louise.crosby@appamondo.com",
-        "username": "louise_crosby",
-        "customerPhone": "+44 9485 472 128",
-        "verified": "Approved"
-      }
+      {
+		"uk": [
+			{
+				"region": "northEast",
+				"location": [
+					{
+						"city": "Newcastle",
+						"areaCode": "NE1",
+						"homes": [
+							{
+								"name": "carehome1",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							},
+							{
+								"name": "carehome2",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							},
+							{
+								"name": "carehome3",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							}
+						]
+					},
+					{
+						"city": "Hartlepool",
+						"areaCode": "TS27",
+						"homes": [
+							{
+								"name": "carehome1",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							},
+							{
+								"name": "carehome2",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							},
+							{
+								"name": "carehome3",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							}
+						]
+					},
+					{
+						"city": "Sunderland",
+						"areaCode": "SR1",
+						"homes": [
+							{
+								"name": "carehome1",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							},
+							{
+								"name": "carehome2",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							},
+							{
+								"name": "carehome3",
+								"postcode": "postcode",
+								"image": "url",
+								"careType": "dementia"
+							}
+						]
+					}
+				]
+			}
+		]
+	}
     })
   };
 }; 
