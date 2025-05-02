81e2453b0f0275d48c81c71e94b34f02c74ed09b
Jack <82877142+jackamondo@users.noreply.github.com>
2025-05-02 12:01:08 +0100

    Update carehomes.js

--- netlify/functions/carehomes.js
+++ netlify/functions/carehomes.js
@@ -1,92 +1,92 @@
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*', // CORS support
-    },
-    body: JSON.stringify({
-      {
-	"uk": [
-	{
-		"region": "northEast",
-		"location": [
-		{
-			"city": "Newcastle",
-			"areaCode": "NE1",
-			"homes": [
-			{
-				"name": "carehome1",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			},
-			{
-				"name": "carehome2",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			},
-			{
-				"name": "carehome3",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			}
-			]
+	return {
+		statusCode: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*', // CORS support
 		},
-		{
-			"city": "Hartlepool",
-			"areaCode": "TS27",
-			"homes": [
+		body: JSON.stringify(
 			{
-				"name": "carehome1",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			},
-			{
-				"name": "carehome2",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			},
-			{
-				"name": "carehome3",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
+				"uk": [
+					{
+						"region": "northEast",
+						"location": [
+							{
+								"city": "Newcastle",
+								"areaCode": "NE1",
+								"homes": [
+									{
+										"name": "carehome1",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									},
+									{
+										"name": "carehome2",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									},
+									{
+										"name": "carehome3",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									}
+								]
+							},
+							{
+								"city": "Hartlepool",
+								"areaCode": "TS27",
+								"homes": [
+									{
+										"name": "carehome1",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									},
+									{
+										"name": "carehome2",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									},
+									{
+										"name": "carehome3",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									}
+								]
+							},
+							{
+								"city": "Sunderland",
+								"areaCode": "SR1",
+								"homes": [
+									{
+										"name": "carehome1",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									},
+									{
+										"name": "carehome2",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									},
+									{
+										"name": "carehome3",
+										"postcode": "postcode",
+										"image": "url",
+										"careType": "dementia"
+									}
+								]
+							}
+						]
+					}
+				]
 			}
-			]
-		},
-		{
-			"city": "Sunderland",
-			"areaCode": "SR1",
-			"homes": [
-			{
-				"name": "carehome1",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			},
-			{
-				"name": "carehome2",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			},
-			{
-				"name": "carehome3",
-				"postcode": "postcode",
-				"image": "url",
-				"careType": "dementia"
-			}
-			]
-		}
-		]
-	}
-	];
+		)
 	};
-    })
-  };
-}; 
+};
