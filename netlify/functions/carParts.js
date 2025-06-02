8f1af939d8a70b9a1c39e6688b2f26a8423e16c2
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-06-02 18:57:06 +0100

    Create carParts.js

---
+++ netlify/functions/carParts.js
@@ -0,0 +1,75 @@
+exports.handler = async (event, context) => {
+	return {
+		statusCode: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*', // CORS support
+		},
+		body: JSON.stringify(
+			{
+				"wiperTypes": [
+					{
+						"type": "Flat Blade",
+						"compatibleCars": [
+							{
+								"make": "Ford",
+								"model": "Focus",
+								"yearRange": "2011–2018"
+							},
+							{
+								"make": "Volkswagen",
+								"model": "Golf",
+								"yearRange": "2013–2019"
+							},
+							{
+								"make": "Audi",
+								"model": "A3",
+								"yearRange": "2010–2016"
+							}
+						]
+					},
+					{
+						"type": "Traditional Frame",
+						"compatibleCars": [
+							{
+								"make": "Toyota",
+								"model": "Corolla",
+								"yearRange": "2005–2012"
+							},
+							{
+								"make": "Honda",
+								"model": "Civic",
+								"yearRange": "2001–2008"
+							},
+							{
+								"make": "Mazda",
+								"model": "3",
+								"yearRange": "2004–2009"
+							}
+						]
+					},
+					{
+						"type": "Hybrid Blade",
+						"compatibleCars": [
+							{
+								"make": "BMW",
+								"model": "3 Series",
+								"yearRange": "2012–2018"
+							},
+							{
+								"make": "Mercedes-Benz",
+								"model": "C-Class",
+								"yearRange": "2014–2020"
+							},
+							{
+								"make": "Lexus",
+								"model": "IS",
+								"yearRange": "2013–2019"
+							}
+						]
+					}
+				]
+			}
+		)
+	};
+};
