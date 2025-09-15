76075a322d8f151723eb7e1a9b801b3879de1b82
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-09-15 09:56:23 +0100

    Create serviceLevel.js

---
+++ netlify/functions/serviceLevel.js
@@ -0,0 +1,22 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // CORS support
+    },
+    body: JSON.stringify({
+      "profile": {
+        "customerName": "Louise Crosby",
+        "customerEmail": "louise.crosby@appamondo.com",
+        "customerPhone": "+44 9485 472 128"
+      },
+      "account": {
+          "plan": {
+            "level": "gold"
+          },
+        "last_updated": "2025-02-25T15:45:00Z"
+      }
+    })
+  };
+}; 
