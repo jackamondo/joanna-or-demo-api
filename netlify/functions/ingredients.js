5676cd443f904d2a769c36e99980e256fa1a84f8
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-09-16 20:18:34 +0100

    Update ingredients.js

--- netlify/functions/ingredients.js
+++ netlify/functions/ingredients.js
@@ -1,1 +1,23 @@
-
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // CORS support
+    },
+    body: JSON.stringify({
+      "healthAndWellbeing": {
+         "gummys": [
+         {
+            "name": "Ashwagandha Gummies",
+            "allergens": "May contain milk, soya, cereals containing gluten, fish and crustaceans"
+        },
+        {
+           "name": "Collagen Gummies",
+           "image": "Fish. May also contain milk, soya, cereals containing gluten, fish and crustaceans"
+       }      
+      ]
+      }
+    })
+  };
+}; 
