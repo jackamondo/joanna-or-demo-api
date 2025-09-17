dc9e76993064281ff85c641966a1e548d1da2d44
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-09-17 07:09:47 +0100

    Create discount.js

---
+++ netlify/functions/discount.js
@@ -0,0 +1,26 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // CORS support
+    },
+    body: JSON.stringify({
+      "discount": {
+         "codes": [
+         {
+            "code": "HELLO15",
+            "conditions": "This code is only valid for new customers and purchase over £50"
+        },
+        {
+           "code": "BOGOFF",
+           "conditions": "This code is only valid when buying two items that are not already discounted"
+       }      
+      ],
+        "newcode": {
+          "new": "fkeiHaqQ39I"
+        }
+      }
+    })
+  };
+}; 
