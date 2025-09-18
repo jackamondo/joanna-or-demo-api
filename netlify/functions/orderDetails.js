a3d59bf05b46cf1c74f68e4157915b883cf635a3
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-09-18 10:53:46 +0100

    Update orderDetails.js

--- netlify/functions/orderDetails.js
+++ netlify/functions/orderDetails.js
@@ -1,6 +1,12 @@
-{
-    "data": {
-        "customer": {
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // CORS support
+    },
+    body: JSON.stringify({
+      "customer": {
             "customerName": "Louise Crosby",
             "customerEmail": "louise.crosby@appamondo.com",
             "customerPhone": "+44 9485 472 128",
@@ -48,3 +54,6 @@
             ]
         }
     },
+    })
+  };
+}; 
