1d512f636616b0b14e4c7eb7e74535a7d88b8124
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-10-18 18:45:57 +0100

    Fix typo in nearest location from 'Farham' to 'Farnham'

--- netlify/functions/location.js
+++ netlify/functions/location.js
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
           "image": "https://asset.nobiadigital.com/image/upload/tr:q-70,f-auto,ar-1.3548-1,c-maintain_ratio,w-840/v1/Magnet/Kitchens/Ambleside/MAG01009_SUMMER_PHASE_ONE_FEB25_SC05_AMBLESIDE_LAKE_S02/?_a=DATAdtAAZAA0"
         },
         {
-          "nearest": "Farham",
+          "nearest": "Farnham",
           "address": "41-42 The Borough Farnham GU9 7NW",
           "image": "https://asset.nobiadigital.com/image/upload/q_auto/f_auto/ar_0.9351,c_fill,w_720/v1/Magnet/Kitchens/Ambleside/MAG002_AMBLESIDE_AUG23_SC02_AMBLESIDE_KITCHEN_MOSS_AND_LIMESTONE_S03_ADOBE_98/?_a=ATAMhAA0"
         },
