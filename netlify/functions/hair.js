35ecedab91d55942672e842a7bb3c0e1453b5ecb
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2025-11-12 15:22:59 +0000

    Refactor customer data structure in hair.js

--- netlify/functions/hair.js
+++ netlify/functions/hair.js
@@ -6,49 +6,47 @@ exports.handler = async (event, context) => {
       'Access-Control-Allow-Origin': '*', // CORS support
     },
     body: JSON.stringify({
-      customer: {
-        customerName: "Louise Crosby",
-        customerEmail: "louise.crosby@appamondo.com",
-        customerPhone: "+44 9485 472 128",
-        billingAddress: {
-          street: "47 Park Lane",
-          city: "London",
-          postCode: "LND 4A2",
-          country: "United Kingdom"
-        },
-        shippingAddress: {
-          street: "47 Park Lane",
-          city: "London",
-          postCode: "LND 4A2",
-          country: "United Kingdom"
-        },
-        order: [
-          {
-            orderNumber: "123456789",
-            orderStatus: "delayed",
-            product: [
-              {
-                productName: "ghd platinum+ styler",
-                productId: 858383,
-                sku: "0406654608",
-                image: "https://testamondo1732886182.zendesk.com/hc/theming_assets/01K9W9BCDMATJG227B7SXTX983",
-                colour: "Black",
-                price: 189.99,
-                quantity: 1
-              },
-              {
-                productName: "rehab split end sealer cream",
-                productId: 858236,
-                sku: "0406634348",
-                image: "https://testamondo1732886182.zendesk.com/hc/theming_assets/01K9W9G56VXX0BGYJM6X0B48GQ",
-                colour: "Gold",
-                price: 28.0,
-                quantity: 1
-              }
+"customer": {
+     "customerName": "Louise Crosby",
+     "customerEmail": "louise.crosby@appamondo.com",
+     "customerPhone": "+44 9485 472 128",
+     "billingAddress": {
+       "street": "47 Park Lane",
+       "city": "London",
+       "postCode": "LND 4A2",
+       "country": "United Kingdom"
+     },
+     "shippingAddress": {
+       "street": "47 Park Lane",
+       "city": "London",
+       "postCode": "LND 4A2",
+       "country": "United Kingdom"
+     },
+     "order": [
+       {
+         "orderNumber": "123456789",
+         "orderStatus": "delayed",
+         "product": [
+           {
+             "productName": "ghd platinum+ styler",
+             "productId": 858383,
+             "sku": "0406654608",
+             "image": "https://testamondo1732886182.zendesk.com/hc/theming_assets/01K9W9BCDMATJG227B7SXTX983,
+             "colour": "Black",
+             "price": 189.99,
+             "quantity": 1
+           },
+           {
+             "productName": "rehab split end sealer cream",
+             "productId": 858236,
+             "sku": "0406634348",
+             "image": "https://testamondo1732886182.zendesk.com/hc/theming_assets/01K9W9G56VXX0BGYJM6X0B48GQ",
+             "colour": "Gold",
+             "price": 28.00,
+             "quantity": 1
+            }
             ]
           }
-        ]
-      }
     })
   };
-};
+}; 
