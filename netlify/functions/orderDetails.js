3eda470448af9ee6e698dc2e64e606d26da45d74
JoannaAppamondo <54098772+JoannaAppamondo@users.noreply.github.com>
2026-02-18 10:42:45 +0000

    Update product details in orderDetails.js

--- netlify/functions/orderDetails.js
+++ netlify/functions/orderDetails.js
@@ -28,25 +28,25 @@ exports.handler = async (event, context) => {
                     "orderStatus": "delayed",
                     "product": [
                         {
-                            "productName": "Pink Jumper",
+                            "productName": "2004-06 England Away Shirt Rooney #9",
                             "productId": 858383,
                             "sku": "0406654608",
-                            "image": "https://testamondo1732886182.zendesk.com/hc/theming_assets/01K5E0Z4XNZXM4YQFEFEYBA9WQ",
-                            "colour": "Pink",
-                            "size": "Womens uk 8",
+                            "image": "https://www.classicfootballshirts.co.uk/cdn-cgi/image/fit=contain,q=70,w=1024,h=1024,f=webp/pub/media/catalog/product//c/e/ce663ebb82f3cddb4329dc5e5cb4f73a1f6f409337a78aa48d225a130b72e696.jpeg",
+                            "colour": "Red",
+                            "size": "M",
                             "weight": 7.5,
-                            "price": 25.99,
+                            "price": 69.99,
                             "quantity": 1
                         },
                         {
-                            "productName": "Blue jeans",
+                            "productName": "2019-20 Barcelona Home Shirt Messi #10 - 10/10 - (S)",
                             "productId": 858236,
                             "sku": "0406634348",
-                            "image": "https://testamondo1732886182.zendesk.com/hc/theming_assets/01K5E11A3MA76QHQVBYR6YA3AG",
+                            "image": "https://www.classicfootballshirts.co.uk/cdn-cgi/image/fit=contain,q=70,w=700,h=700,f=webp/pub/media/catalog/product//0/4/040226912033-1_weirjunjr19flzdk.jpg",
                             "colour": "Blue",
-                            "size": "Womens uk 8",
+                            "size": "S",
                             "weight": 0.6,
-                            "price": 35.99,
+                            "price": 89.99,
                             "quantity": 1
                         }
                     ]
